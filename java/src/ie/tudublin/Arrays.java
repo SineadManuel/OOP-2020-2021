166c854adead5c387d74d0ba94283ee1a4e4d2eb
Sinead-Manuel <08sinead.manuel01@gmail.com>
2021-02-18 10:03:58 +0000

    Charts with Maps & Arrays

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -58,7 +58,7 @@ public class Arrays extends PApplet {
     int mode = 0;
 
     float[] rainfall = { 45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58 };
-    String[] months = { "Jan", "Feb", "March", "April", "May", "June", "July", "August", "Sept", "Oct", "Nov", "Dec" };
+    String[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
     float[] arr = new float[100]; // 100 float array
 
     public void keyPressed() {
@@ -111,28 +111,149 @@ public class Arrays extends PApplet {
         // Use the map function
 
         colorMode(HSB);
-        float w = width / (float) rainfall.length;
+        float w = 400 / (float) rainfall.length;
         for (int i = 0; i < rainfall.length; i++) {
-            noStroke();
+            stroke(255);
             fill(random(255), 255, 255);
-            float x = map(i, 0, rainfall.length, 0, width);
-            rect(x, height, w, -rainfall[i]);
+            float x = map(i, 0, rainfall.length, 50, 450);
+            rect(x, 450, w, -rainfall[i]);
         }
     }
 
 
     public void draw() {
         background(0);
+        size(500, 500);
         switch (mode) {
-            case 0: {
-                // Bar chart
+            // Bar Chart
+            case 0: {               
+                // Y-axis numbers
+                float border = width * 0.1f;
+                for (int i = 120; i >= 0; i--) {
+                    float x = map(i, 120, 1.5f, border + 5 , width - border);
+                    fill(255);
+                    text(i, border * 0.5f, x);
+                    i -= 9;
+                }
+                
+                // Y-axis markers
+                for (int i = 120; i >= 0; i--) {
+                    float x = map(i, 120, 0, border, width - border);
+                    fill(255);
+                    line(45, x, 50, x);
+                    i -= 9;
+                }
+                
+                // X-axis month lables
+                for (int i = 0; i < rainfall.length; i++) {
+                    fill(255);
+                    float x = map(i, 0, rainfall.length, 65, 470);
+                    text(months[i], x, 475);
+                }
+
+                fill(255);
+                // Chart title
+                text("Rainfall Bar Chart", 250, 30);
+                textAlign(CENTER);
+                line(50, 50, 50, 450);
+
+                // Colourful bars
+                colorMode(HSB);
+                float w = 400 / (float) rainfall.length;
+                for (int i = 0; i < rainfall.length; i++) {
+                    stroke(255);
+                    fill(i * 21, 255, 255);
+                    float x = map(i, 0, rainfall.length, 50, 450);
+                    
+                    // Bryan's code
+                    float h = map(rainfall[i], 0, 120, 0, -(height - (border * 2)));
+                    rect(x, height - border - 1, w, h);
+
+                    // My code
+                    //rect(x, 450, w, -rainfall[i] * 3.3f);
+                }
                 break;
             }
+            // Trend line
             case 1: {
+                // Y-axis numbers
+                float border = width * 0.1f;
+                for (int i = 120; i >= 0; i--) {
+                    float x = map(i, 120, 1.5f, border + 5 , width - border);
+                    fill(255);
+                    text(i, border * 0.5f, x);
+                    i -= 9;
+                }
+                
+                // Y-axis markers
+                for (int i = 120; i >= 0; i--) {
+                    float x = map(i, 120, 0, border, width - border);
+                    fill(255);
+                    line(45, x, 50, x);
+                    i -= 9;
+                }
+                
+                // X-axis month lables
+                for (int i = 0; i < rainfall.length; i++) {
+                    fill(255);
+                    float x = map(i, 0, rainfall.length, 65, 470);
+                    text(months[i], x, 475);
+                }
+
+                fill(255);
+                // Chart title
+                text("Rainfall Trend Chart", 250, 30);
+                textAlign(CENTER);
+                
+                // Axis boundaries
+                int minSize = 50;
+                int maxSize = 450;
+                
+                // Y-axis line
+                line(minSize, minSize, minSize, maxSize);
+
+                // X-axis line
+                line(minSize, maxSize, maxSize, maxSize);
+
                 // Trend line
+                for (int i = 1; i < rainfall.length; i++) {
+                    stroke(255);
+                    float x = map(i, 0, rainfall.length, 30, 440);
+                    line(x, maxSize - (rainfall[i - 1] * 3.3f), x + 34, maxSize - (rainfall[i] * 3.3f));
+                }
+                break;
             }
+            // Pie chart
             case 2: {
-                // Pie chart
+                fill(255);
+                // Chart title
+                text("Rainfall Pie Chart", 250, 30);
+                textAlign(CENTER);
+
+                float sum = 0;
+                int minIndex = 0;
+                int maxIndex = 0;
+
+                // Calculating sum of anual temp
+                for (int i = 0; i < rainfall.length; i++) {
+                    if (rainfall[i] < rainfall[minIndex]) {
+                        minIndex = i;
+                    }
+                    if (rainfall[i] > rainfall[maxIndex]) {
+                        maxIndex = i;
+                    }
+                    sum += rainfall[i];
+                }
+                
+                colorMode(HSB);
+                for (int i = 0; i < rainfall.length; i++) {
+                    stroke(255);
+                    float angle = map(rainfall[i], 0, sum, 0, TWO_PI);
+                    fill(i * 21, 255, 255);
+                    arc(250, 250, 250, 250, 0, angle);
+                    
+                }
+                break;
             }
         }
     }
